46786bd81cb396e242e53e74b370337fe73da621
JohnAsare <johnasare7@gmail.com>
2020-06-16 18:30:20 -0400

    Update the docu

--- functionsAndMethods.py
+++ functionsAndMethods.py
@@ -4,11 +4,12 @@
 
 """
 LESSER OF TWO EVENS: Write a function that returns the lesser of two given numbers if both numbers are
-even, but returns the greater if one or both numbers are odd"""
+even, but returns the greater if one or both numbers are odd
+lesser_of_two_evens(2,4) --> 2
+lesser_of_two_evens(2,5) --> 5 """
 
 
 def lesser_of_two_evens(a, b):
-
 
 
 
