892bd6b2406c208909e7da560e13b09e3e4e8512
JohnAsare <johnasare7@gmail.com>
2020-06-16 18:42:21 -0400

    Add docu of the Animal Crackers code function

---
+++ animalCrackers.py
@@ -0,0 +1,9 @@
+# John Asare
+# Jun 16 2020
+
+
+""" ANIMAL CRACKERS: Write a function takes a two-word string and returns True if both words begin with
+same letter
+animal_crackers('Levelheaded Llama') --> True
+animal_crackers('Crazy Kangaroo') --> False """
+
