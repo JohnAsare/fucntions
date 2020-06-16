f9997af156ab7940393468f38c808b780e5cee06
JohnAsare <johnasare7@gmail.com>
2020-06-16 19:54:42 -0400

    Add docu for the old mac fucntion

---
+++ oldmac.py
@@ -0,0 +1,13 @@
+# John Asare
+# Jun 16 2020
+
+
+""" OLD MACDONALD: Write a function that capitalizes the first and fourth letters of a name
+old_macdonald('macdonald') --> MacDonald """
+
+
+def old_macdonald(name):
+    return name.capitalize()
+
+
+print(old_macdonald('macdonald'))
