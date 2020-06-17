41d62f2f82a080d6f6aaf397364c95861444cf14
JohnAsare <johnasare7@gmail.com>
2020-06-16 20:25:41 -0400

    Add docu statement for the makestwenty

---
+++ makesTwenty.py
@@ -0,0 +1,17 @@
+# John Asare
+# Jun 16 2020
+
+
+""" MAKES TWENTY: Given two integers, return True if the sum of the integers is 20 or if one of the integers
+ is 20. If not, return False
+makes_twenty(20,10) --> True
+makes_twenty(12,8) --> True
+makes_twenty(2,3) --> False """
+
+
+def makes_twenty(n1, n2):
+
+
+
+makes_twenty(20,10)
+makes_twenty(2,3)
