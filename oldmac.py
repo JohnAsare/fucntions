afadf59963988462393ca1f713c7f2f16a7219c1
JohnAsare <johnasare7@gmail.com>
2020-06-16 20:20:57 -0400

    Use indice and slicing to reformat a string by capitalizing the first and fourth letter

--- oldmac.py
+++ oldmac.py
@@ -7,7 +7,12 @@ old_macdonald('macdonald') --> MacDonald """
 
 
 def old_macdonald(name):
-    return name.capitalize()
+    upper_first = name[0].capitalize()
+    middle = name[1:3]
+    upper_fourth = name[3].capitalize()
+    rest = name[4:]
+
+    return upper_first + middle + upper_fourth + rest
 
 
 print(old_macdonald('macdonald'))
