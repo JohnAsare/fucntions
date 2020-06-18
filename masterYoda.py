8170692355539ec55de15a3006295066ebf204c1
JohnAsare <johnasare7@gmail.com>
2020-06-17 22:30:09 -0400

    Add docu for the functions

---
+++ masterYoda.py
@@ -0,0 +1,15 @@
+# John Asare
+# Jun 17 2020
+
+
+"""
+MASTER YODA: Given a sentence, return a sentence with the words reversed
+master_yoda('I am home') --> 'home am I'
+master_yoda('We are ready') --> 'ready are We' """
+
+
+def master_yoda(text):
+    pass
+
+master_yoda('I am home')
+master_yoda('We are ready')
