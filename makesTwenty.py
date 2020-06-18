b338a395c16d7d75545e9673f7afa513b52e5799
JohnAsare <johnasare7@gmail.com>
2020-06-17 21:55:03 -0400

    Use boolean to check if two numbers are 20 or one of them is 20

--- makesTwenty.py
+++ makesTwenty.py
@@ -12,9 +12,13 @@ makes_twenty(2,3) --> False """
 def makes_twenty(n1, n2):
     if sum((n1, n2)) == 20:
         return True
+    elif n1 == 20 or n2 == 20:
+        return True
     else:
         return False
+
 
 print(makes_twenty(20, 10))
 print(makes_twenty(12, 8))
 print(makes_twenty(2, 3))
+
