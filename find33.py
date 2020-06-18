2c2cda3d79618632997c46843bbe3063804fe2ec
JohnAsare <johnasare7@gmail.com>
2020-06-18 12:18:39 -0400

    Add a python file to compile the code

---
+++ find33.py
@@ -0,0 +1,17 @@
+# John Asare
+# Jun 18 2020
+
+
+""" Given a list of ints, return True if the array contains a 3 next to a 3 somewhere.
+has_33([1, 3, 3]) → True
+has_33([1, 3, 1, 3]) → False
+has_33([3, 1, 3]) → False """
+
+
+def has_33(nums):
+    pass
+
+
+has_33([1, 3, 3])
+has_33([1, 3, 1, 3])
+has_33([3, 1, 3])
