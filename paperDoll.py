3ea08e0cac391fafbedd3d675871242487040cee
JohnAsare <johnasare7@gmail.com>
2020-06-18 17:42:46 -0400

    Add python file, docu and the fucntion with pass as the method

---
+++ paperDoll.py
@@ -0,0 +1,13 @@
+# John Asare
+# Jun 18 2020
+
+"""
+PAPER DOLL: Given a string, return a string where for every character in the original there are three characters
+paper_doll('Hello') --> 'HHHeeellllllooo'
+paper_doll('Mississippi') --> 'MMMiiissssssiiippppppiii' """
+
+def paper_doll(text):
+    pass
+
+paper_doll('Hello')
+paper_doll('Mississippi')
