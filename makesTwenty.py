875cc7616dfb369f7a6db05bb45c57a2457164e8
JohnAsare <johnasare7@gmail.com>
2020-06-17 21:41:29 -0400

    Use sum keyword to check if two numbers equal 20

--- makesTwenty.py
+++ makesTwenty.py
@@ -10,18 +10,10 @@ makes_twenty(2,3) --> False """
 
 
 def makes_twenty(n1, n2):
-    total = sum((n1, n2))
-    print(total)
-
-    if total == (n1 + n2):
-      return
-    if total or n1 == 20:
-        return True
-    elif total or n2 == 20:
+    if sum((n1, n2)) == 20:
         return True
     else:
-       return False
-
+        return False
 
 print(makes_twenty(20, 10))
 print(makes_twenty(12, 8))
